3bb58feeb00dec87091e1af062317b9840a55811
Angel Terrones <angelterrones@gmail.com>
2015-07-07 21:04:34 -0430

    Fist commit

---
+++ i3/i3lock.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+IMAGE=/home/angelterrones/Pictures/Wallpapers/zf1f7OZ.png
+#scrot /tmp/screen_locked.png
+#convert /tmp/screen_locked.png -blur 2x2 /tmp/screen_locked2.png
+i3lock -i $IMAGE -t
