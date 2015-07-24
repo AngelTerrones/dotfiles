0197c622997bccd106361c5627431f0db0813e44
Angel Terrones <angelterrones@gmail.com>
2015-07-24 00:11:28 -0430

    Add C/C++ templates

---
+++ vim/templates/skeleton.hpp
@@ -0,0 +1,16 @@
+//==================================================================================================
+//  Filename      : <filename>
+//  Created On    : <date>
+//  Revision      : 0.1
+//  Author        : Angel Terrones
+//  Company       : Universidad Simón Bolívar
+//  Email         : aterrones@usb.ve
+//  Description   :
+//==================================================================================================
+
+#ifndef <fileguard>
+#define <fileguard>
+
+
+
+#endif // <fileguard>
