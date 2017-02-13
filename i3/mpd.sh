65c9b5aeebae54c895d879e74db719e32f0c7041
Angel Terrones <angelterrones@gmail.com>
2017-02-12 20:57:14 -0400

    Improve mpd status

--- i3/mpd.sh
+++ i3/mpd.sh
@@ -3,7 +3,14 @@ msg=$({ echo "status"; sleep 0.1; echo "currentsong"; sleep 0.1; echo "close"; }
 
 state=$(echo "${msg}" | sed -n 's/state: //p')
 artist=$(echo "${msg}" | sed -n 's/^Artist: //p')
-title=$(echo "${msg}" | sed -n 's/Title: //p')
+title=$(echo "${msg}" | sed -n 's/Title: //p' | sed -n 's/\\/-/p') # Replace \ with -
+
+if [ -z $artist ] ; then
+	artist=$(echo Unknown artist)
+fi
+if [ -z $title ] ; then
+	artist=$(echo Unknown song)
+fi
 
 if [[ ${state} = "play" ]]; then
     echo "♫ ${artist} : ${title}"
