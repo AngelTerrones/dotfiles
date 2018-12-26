340a1936945e3818e9aeb16c6d4353c0e71f64ad
Angel Terrones <angelterrones@gmail.com>
2018-12-26 01:13:45 -0400

    Update config for desktop.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,4 +7,11 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-polybar topbar &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload topbar &
+  done
+else
+  polybar --reload topbar &
+fi
