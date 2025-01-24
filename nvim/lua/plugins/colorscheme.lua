9f31e278f43350c6b692be89ed0e1d5f8f43607a
Angel Terrones <angelterrones@gmail.com>
2025-01-24 13:36:59 -0400

    Update theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,17 @@
 return {
 	{
-		"sharpchen/Eva-Theme.nvim",
-		lazy = false,
-		priority = 1000,
-		build = ":EvaCompile",
+		"rebelot/kanagawa.nvim",
 		config = function()
-			vim.cmd("colorscheme Eva-Dark-Bold")
+			vim.cmd("colorscheme kanagawa-wave") -- dragon?
 		end,
 	},
+	-- {
+	-- 	"sharpchen/Eva-Theme.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	build = ":EvaCompile",
+	-- 	config = function()
+	-- 		vim.cmd("colorscheme Eva-Dark-Bold")
+	-- 	end,
+	-- },
 }
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,7 +3,8 @@ return {
 		"nvim-lualine/lualine.nvim",
 		opts = {
 			options = {
-				theme = "onedark",
+				--theme = "onedark",
+				theme = "auto",
 				component_separators = "",
 				section_separators = { left = "", right = "" },
 			},
